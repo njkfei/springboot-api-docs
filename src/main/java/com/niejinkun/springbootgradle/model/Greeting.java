d4e429e03a1fb39b5ca6683414fea4e89daf38c0
爱折腾 <297206021@qq.com>
2017-12-07 17:49:06 +0800

    Update Greeting.java

--- src/main/java/com/niejinkun/springbootgradle/model/Greeting.java
+++ src/main/java/com/niejinkun/springbootgradle/model/Greeting.java
@@ -1,7 +1,9 @@
 package com.niejinkun.springbootgradle.model;
 
 public class Greeting {
+   @ApiModelProperty(value = "id", required = true)
   private final long id;
+  @ApiModelProperty(value = "content", required = true)
   private final String content;
   public Greeting(long id, String content) {
     this.id = id;
